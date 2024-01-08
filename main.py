55ae9a59f8991e95c5abae2b81402021a7017107
cmylucas <155979046+cmylucas@users.noreply.github.com>
2024-01-08 08:30:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-# This Python 3 environment comes with many helpful analytics libraries installed
-# It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load
-
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import glob
@@ -24,17 +20,7 @@ import pickle
 
 nltk.download('stopwords')
 
-# Input data files are available in the read-only "../input/" directory
-# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
-
 import os
-# for dirname, _, filenames in os.walk('./'):
-#     for filename in filenames:
-#         print(os.path.join(dirname, filename))
-
-# You can write up to 20GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All" 
-# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
-
 
 flist = glob.glob('mail/*.txt')
 mails = []
@@ -86,18 +72,6 @@ plot_word_cloud(mails[mails['spam'] == 1], typ='Spam')
 
 linkcounter(mails)
 
-# c=0
-# x=0
-# while (c <10):
-#     # print(x)
-#     if mails.loc[x]['spam'] == 1:
-#         print("Real")
-#         print(mails.loc[c]['text'])
-#         print("Spam")
-#         print(mails.loc[x]['text'])
-#         c += 1
-#     x += 1
-
 def remove_stopwords(text):
     stop_words = stopwords.words('english')
     imp_words = []
@@ -118,18 +92,6 @@ mails['text'] = mails['text'].str.replace('Subject', '')
 punctuations_list = string.punctuation
 mails['text']= mails['text'].apply(lambda x: remove_punctuations(x))
 mails['text'] = mails['text'].apply(lambda text: remove_stopwords(text))
-
-# c=0
-# x=0
-# while (c <10):
-#     # print(x)
-#     if mails.loc[x]['spam'] == 1:
-#         print("Real")
-#         print(mails.loc[c]['text'])
-#         print("Spam")
-#         print(mails.loc[x]['text'])
-#         c += 1
-#     x += 1
 
 
 cv = CountVectorizer(decode_error='ignore')
